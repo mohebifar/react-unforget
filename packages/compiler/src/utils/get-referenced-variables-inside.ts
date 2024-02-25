cd7945a3e3d931ff9ec9fe6b30017141309985b0
Mohamad Mohebifar <mohamad@mohebifar.com>
2024-02-25 09:46:08 -0500

    enhancement: Improve reliability of getReferencedVariablesInside

--- packages/compiler/src/utils/get-referenced-variables-inside.ts
+++ packages/compiler/src/utils/get-referenced-variables-inside.ts
@@ -8,17 +8,13 @@ export function getReferencedVariablesInside(
   path.traverse({
     Identifier(innerPath) {
       // Check if the identifier is a reference to a variable
-      // Skip over keys in object properties and function/method names
+      // Skip over keys in object properties, function/method names, etc.
       if (
-        !innerPath.parentPath.isObjectProperty({ key: innerPath.node }) &&
-        !innerPath.parentPath.isMemberExpression({
-          property: innerPath.node,
-        }) &&
-        !innerPath.parentPath.isFunctionDeclaration({ id: innerPath.node }) &&
-        !innerPath.parentPath.isFunctionExpression({ id: innerPath.node }) &&
-        !innerPath.parentPath.isClassMethod({ key: innerPath.node }) &&
-        !innerPath.parentPath.isClassProperty({ key: innerPath.node }) &&
-        !innerPath.parentPath.isVariableDeclarator({ id: innerPath.node })
+        objectPropsCheck(innerPath) &&
+        functionIdCheck(innerPath) &&
+        classDeclarationCheck(innerPath) &&
+        variableDeclaratorCheck(innerPath) &&
+        memberExpressionCheck(innerPath)
       ) {
         references.add(innerPath.node.name);
       }
@@ -35,4 +31,67 @@ export function getReferencedVariablesInside(
   });
 
   return bindings;
+}
+
+function variableDeclaratorCheck(path: babel.NodePath<babel.types.Identifier>) {
+  if (
+    path.parentPath.isVariableDeclarator({
+      id: path.node,
+    })
+  ) {
+    return false;
+  }
+
+  // For array patterns and object patterns, we want to check if the identifier is part of the id not init
+  const parentVariableDeclarator = path.findParent((p) =>
+    p.isVariableDeclarator()
+  );
+  const parentVariableDeclaratorId = parentVariableDeclarator?.get("id") as
+    | babel.NodePath<babel.types.Node>
+    | undefined;
+
+  return (
+    !parentVariableDeclaratorId ||
+    !path.isDescendant(parentVariableDeclaratorId)
+  );
+}
+
+function memberExpressionCheck(path: babel.NodePath<babel.types.Identifier>) {
+  return !path.parentPath.isMemberExpression({
+    property: path.node,
+    computed: false,
+  });
+}
+
+function functionIdCheck(path: babel.NodePath<babel.types.Identifier>) {
+  return (
+    !path.parentPath.isFunctionDeclaration({
+      id: path.node,
+    }) &&
+    !path.parentPath.isFunctionExpression({
+      id: path.node,
+    })
+  );
+}
+
+function classDeclarationCheck(path: babel.NodePath<babel.types.Identifier>) {
+  return !path.parentPath.isClassDeclaration({
+    id: path.node,
+  });
+}
+
+function objectPropsCheck(path: babel.NodePath<babel.types.Identifier>) {
+  return (
+    !path.parentPath.isClassProperty({
+      key: path.node,
+      computed: false,
+    }) &&
+    !path.parentPath.isObjectProperty({ key: path.node, computed: false }) &&
+    !path.parentPath.isClassMethod({ key: path.node, computed: false }) &&
+    !path.parentPath.isObjectMethod({ key: path.node, computed: false }) &&
+    !path.parentPath.isClassPrivateProperty({
+      key: path.node,
+      computed: false,
+    })
+  );
 }
--- packages/compiler/src/utils/tests/get-referenced-variables-inside.test.ts
+++ packages/compiler/src/utils/tests/get-referenced-variables-inside.test.ts
@@ -1,66 +1,235 @@
 import { getReferencedVariablesInside } from "../get-referenced-variables-inside";
 import { parse } from "../testing";
 
+const parseCodeForTest = (code: string) => {
+  const programPath = parse(code);
+
+  const programBody = programPath.get("body");
+
+  return programBody;
+};
+
+const getFunctionFromBodyPath = (
+  programBody: babel.NodePath<babel.types.Statement>[],
+  componentName = "MyComponent"
+) => {
+  const fn = programBody.find(
+    (p) => p.isFunctionDeclaration() && p.node.id?.name === componentName
+  ) as babel.NodePath<babel.types.FunctionDeclaration>;
+
+  return fn;
+};
+
+const getReturnStatement = (fn: babel.NodePath<babel.types.Function>) => {
+  return fn.get("body.body").find((p) => {
+    return p.isReturnStatement();
+  });
+};
+
 describe("getReferencedVariablesInside", () => {
   it("basic example", () => {
-    const path = parse(`
+    const body = parseCodeForTest(`
   const myVariable = 2;
-  const MyComponent = () => {
+  function MyComponent() {
     return myVariable;
   };
   `);
 
-    const bindings = getReferencedVariablesInside(
-      path.get("body.1.declarations.0.init.body") as any
-    );
+    const fn = getFunctionFromBodyPath(body);
+    const returnStatement = getReturnStatement(fn)!;
+    const bindings = getReferencedVariablesInside(returnStatement);
 
     expect(bindings).toHaveLength(1);
-    expect(bindings[0]?.path).toStrictEqual(path.get("body.0.declarations.0"));
+    expect(bindings[0]?.path).toEqual(body.at(0)?.get("declarations.0"));
   });
 
   it("returns referenced values inside the given node only", () => {
-    const path = parse(`
+    const body = parseCodeForTest(`
   const myVariable = 2;
-  const MyComponent = () => {
+  function MyComponent() {
     const derivedValue = myVariable * 2;
     return derivedValue;
   };
   `);
 
-    const functionBody = path.get("body.1.declarations.0.init.body") as any;
-
-    const returnStatement = functionBody.get("body.1") as any;
-
+    const fn = getFunctionFromBodyPath(body);
+    const returnStatement = getReturnStatement(fn)!;
     const bindings = getReferencedVariablesInside(returnStatement);
 
     expect(bindings).toHaveLength(1);
     expect(bindings[0]?.identifier.name).toStrictEqual("derivedValue");
     expect(bindings[0]?.path).toStrictEqual(
-      functionBody.get("body.0.declarations.0")
+      fn.get("body.body.0.declarations.0")
     );
   });
 
   it("returns multiple referenced values inside the given node only", () => {
-    const path = parse(`
+    const body = parseCodeForTest(`
   const myVariable = 2;
-  const MyComponent = () => {
+  function MyComponent() {
     const derivedValue = myVariable * 2;
     return derivedValue + myVariable;
   };
   `);
 
-    const functionBody = path.get("body.1.declarations.0.init.body") as any;
-
-    const returnStatement = functionBody.get("body.1") as any;
-
+    const fn = getFunctionFromBodyPath(body);
+    const returnStatement = getReturnStatement(fn)!;
     const bindings = getReferencedVariablesInside(returnStatement);
 
     expect(bindings).toHaveLength(2);
     expect(bindings[0]?.identifier.name).toStrictEqual("derivedValue");
     expect(bindings[0]?.path).toStrictEqual(
-      functionBody.get("body.0.declarations.0")
+      fn.get("body.body.0.declarations.0")
     );
     expect(bindings[1]?.identifier.name).toStrictEqual("myVariable");
-    expect(bindings[1]?.path).toStrictEqual(path.get("body.0.declarations.0"));
+    expect(bindings[1]?.path).toStrictEqual(body.at(0)?.get("declarations.0"));
+  });
+
+  it("properly handles member expressions", () => {
+    const body = parseCodeForTest(`
+const key = "key";
+const num = 2;
+const obj = {};
+function MyComponent() {
+  return obj.foo[key].bar[num];
+};
+  `);
+    const fn = getFunctionFromBodyPath(body);
+    const returnStatement = getReturnStatement(fn)!;
+    const bindings = getReferencedVariablesInside(returnStatement);
+
+    // expect(bindings).toHaveLength(3);
+    expect(bindings.map((b) => b.identifier.name)).toStrictEqual([
+      "obj",
+      "key",
+      "num",
+    ]);
+  });
+
+  it("properly handles call expressions", () => {
+    const body = parseCodeForTest(`
+const key = "key";
+const num = 2;
+const fn = {};
+function MyComponent() {
+  return fn(key, num);
+};
+  `);
+    const fn = getFunctionFromBodyPath(body);
+    const returnStatement = getReturnStatement(fn)!;
+    const bindings = getReferencedVariablesInside(returnStatement);
+
+    expect(bindings).toHaveLength(3);
+    expect(bindings.map((b) => b.identifier.name)).toStrictEqual([
+      "fn",
+      "key",
+      "num",
+    ]);
+  });
+
+  it("properly handles call expressions with member expression callee", () => {
+    const body = parseCodeForTest(`
+const key = "key";
+const num = 2;
+const fn = {};
+function MyComponent() {
+  return fn.useHook(key, num);
+};
+  `);
+    const fn = getFunctionFromBodyPath(body);
+    const returnStatement = getReturnStatement(fn)!;
+    const bindings = getReferencedVariablesInside(returnStatement);
+
+    expect(bindings).toHaveLength(3);
+    expect(bindings.map((b) => b.identifier.name)).toStrictEqual([
+      "fn",
+      "key",
+      "num",
+    ]);
+  });
+
+  describe("exclusion", () => {
+    it("does not count function delcaration", () => {
+      const body = parseCodeForTest(`
+function MyComponent() {
+  function innerComponent() {}
+};
+    `);
+
+      const fn = getFunctionFromBodyPath(body);
+      const bindings = getReferencedVariablesInside(fn);
+
+      expect(bindings).toHaveLength(0);
+    });
+
+    it("does not count variable declarator ids", () => {
+      const body = parseCodeForTest(`
+function MyComponent() {
+  const declaredVar = 2;
+  const [a, b] = [1, 2];
+  const { c, d } = { c: 1, d: 2 };
+};
+    `);
+
+      const fn = getFunctionFromBodyPath(body);
+      const bindings = getReferencedVariablesInside(fn);
+
+      expect(bindings).toHaveLength(0);
+    });
+
+    it("does not count class, and its properties and methods", () => {
+      const body = parseCodeForTest(`
+const property = "dummy-value";
+const method = "dummy-value";
+const computedProperty = "dummy-value";
+const computedMethod = "dummy-value";
+
+function MyComponent() {
+  class MyClass {
+    method() {}
+    property = 2;
+    [computedProperty] = 3;
+    [computedMethod]() {}
+  }
+};
+    `);
+
+      const fn = getFunctionFromBodyPath(body);
+      const bindings = getReferencedVariablesInside(fn);
+
+      expect(bindings.map((b) => b.identifier.name)).toStrictEqual([
+        "computedProperty",
+        "computedMethod",
+      ]);
+    });
+
+    it("does not count object property keys", () => {
+      const body = parseCodeForTest(`
+const property = "dummy-value";
+const method = "dummy-value";
+const computedProperty = "dummy-value";
+const computedMethod = "dummy-value";
+const referencedValue = "dummy-value";
+
+function MyComponent() {
+  const obj = {
+    method() {},
+    property: 2,
+    [computedProperty]: 3,
+    [computedMethod]() {},
+    prop: referencedValue.test
+  };
+};
+    `);
+
+      const fn = getFunctionFromBodyPath(body);
+      const bindings = getReferencedVariablesInside(fn);
+
+      expect(bindings.map((b) => b.identifier.name)).toStrictEqual([
+        "computedProperty",
+        "computedMethod",
+        "referencedValue",
+      ]);
+    });
   });
 });
